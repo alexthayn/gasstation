2906145930b7871a3b8ca328f452e4962d89ded8
Jackson Porter (JP) <jackson.porter@students.snow.edu>
2018-02-04 22:37:19 -0700

    Created an Eclipse Project

--- GasStation/GasStation.java
+++ GasStation/GasStation.java
@@ -1,6 +1,7 @@
+package GasStation;
 public class GasStation{
 
 	public static void main(String[] args){
-		System.out.println("Eat my shorts... Alex");
+		System.out.println("Eat my shorts... tomorrow");
 	}
 }
