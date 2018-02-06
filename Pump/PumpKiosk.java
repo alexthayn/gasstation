c097d5ff053500bd5240699a7f8de3d18bd3c93d
Jackson Porter <jackson.porter@ad.snow.edu>
2018-02-06 14:22:53 -0700

    moved commands to pumpkiosk

--- Pump/PumpKiosk.java
+++ Pump/PumpKiosk.java
@@ -1,7 +1,12 @@
 package Pump;
 
 public class PumpKiosk implements Runnable{
-
+	//Global variables
+	PumpDieselCommand dieselCommand;
+	PumpMidGradeCommand midGradeCommand;
+	PumpPremiumCommand premiumCommand;
+	PumpUnleadedCommand unleadedCommand;
+	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
