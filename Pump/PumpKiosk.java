ddc4bc0e5ab8cc559ffe1b55db1c949fe289e763
TyBayn <tymcb@hotmail.com>
2018-02-06 14:23:44 -0700

    Working on PumpKiosk

--- Pump/PumpKiosk.java
+++ Pump/PumpKiosk.java
@@ -2,6 +2,11 @@ package Pump;
 
 public class PumpKiosk implements Runnable{
 
+	private long dieselCost;
+	private long premCost;
+	private long midCost;
+	private long unlCost;
+	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
