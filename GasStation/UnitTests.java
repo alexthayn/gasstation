f2bcd486998861183ab760f6b50a9aa09de13641
alexthayn <alexat789@gmail.com>
2018-02-07 09:26:00 -0700

    test cases added

---
+++ GasStation/UnitTests.java
@@ -0,0 +1,69 @@
+package GasStation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import Pump.Pump;
+import Pump.PumpKiosk;
+import Pump.PumpThread;
+import Tank.DieselTank;
+import Tank.PremiumTank;
+import Tank.Tank;
+
+class UnitTests {
+	Tank premiumTank = PremiumTank.getTank();
+	Tank dieselTank = DieselTank.getTank();
+	
+	@ParameterizedTest(name="{index}=>type= {0}, amount = {1}, fuelRemaining = {2}")
+	@CsvSource({
+		"Premium, 12, 188",
+		"Premium, 25, 363",
+		"Premium, 100, 463",
+		"Premium, 150, 513",
+		//Edge cases
+		"Premium, 0, 713",
+		"Premium, 800, 113"
+	})
+	void premiumTankTest(String type, double amount, double fuelRemaining) {
+		try {
+			
+			premiumTank.refill(200);
+			
+			premiumTank.remove(amount);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e);
+		}
+		System.out.println("The amount remaining in the premium tank is: " + premiumTank.getAmount());
+		assertEquals(fuelRemaining, premiumTank.getAmount(), .5);
+	}
+
+	@ParameterizedTest(name="{index}=>type= {0}, amount = {1}, fuelRemaining = {2}")
+	@CsvSource({
+		"Diesel, 12, 188",
+		"Diesel, 25, 363",
+		"Diesel, 100, 463",
+		"Diesel, 150, 513",
+		//Edge cases
+		"Diesel, 0, 713",
+		"Diesel, 800, 113"
+	})
+	void dieselTankTest(String type, double amount, double fuelRemaining) {
+		try {
+			dieselTank.refill(200);
+			
+			dieselTank.remove(amount);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e);
+		}
+		System.out.println("The amount remaining in the diesel tank is: " + dieselTank.getAmount());
+		assertEquals(fuelRemaining, dieselTank.getAmount(), .001);
+		
+	}
+}
